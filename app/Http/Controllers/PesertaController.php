0bfe9c6523e5e5ed8fb5ffc7ed784a128c9cd6f6
syahrizall <farhan.rachmat09@gmail.com>
2022-08-16 19:19:53 +0700

    create service getDataAlamat

--- app/Http/Controllers/PesertaController.php
+++ app/Http/Controllers/PesertaController.php
@@ -101,7 +101,7 @@ class PesertaController extends Controller
         return redirect('/admin/peserta')->with('message', 'Data Berhasil Dihapus');
     }
     public function getDataAlamat(Request $request){
-        $data = DB::table('m_desa_kelurahan')->select('m_desa_kelurahan.id as id_desa_kelurahan', 'm_desa_kelurahan.nama_desa_kelurahan as nama_desa_kelurahan', 'm_kecamatan.nama_kecamatan as nama_kecamatan', 'm_kota_kab.nama as nama_kota_kab', 'm_provinsi.nama as nama_provinsi')
+        $data = DB::table('m_desa_kelurahan')->select('m_desa_kelurahan.id as id_desa_kelurahan', 'm_desa_kelurahan.nama_desa_kelurahan as nama_desa_kelurahan', 'm_kecamatan.id as id_kecamatan', 'm_kecamatan.nama_kecamatan as nama_kecamatan', 'm_kota_kab.id as id_kota_kab', 'm_kota_kab.nama as nama_kota_kab', 'm_provinsi.id as id_provinsi', 'm_provinsi.nama as nama_provinsi')
         ->join('m_kecamatan', 'm_kecamatan.id', '=', 'm_desa_kelurahan.id_kecamatan')
         ->join('m_kota_kab', 'm_kota_kab.id', '=', 'm_kecamatan.id_kota_kabupaten')
         ->join('m_provinsi', 'm_provinsi.id', '=', 'm_kota_kab.provinsi_id')
